15c3be78d7dda0240ebd08d7636191051423b131
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-05-16 15:22:04 +0600

    up to date

--- dbConnection.php
+++ dbConnection.php
@@ -9,10 +9,11 @@ $database_name = "landing_page";
 // $password = "_^Mlr+NnZ=ga";
 // $database_name = "easytechx_landing_page";
 
-$conn = mysqli_connect($servername, $username, $password, $database_name);
+$conn = new mysqli($servername, $username, $password, $database_name);
+$conn->set_charset("utf8mb4");
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 ?>
